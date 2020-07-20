d578a07dc9fd39bd08bef5d3b3bcb1277dee9f24
oevl <omar.vasquezlima@gmail.com>
2020-07-20 10:22:47 -0500

    Working Code page element component

--- root-wrapper.js
+++ root-wrapper.js
@@ -1,45 +1,29 @@
 import { MDXProvider } from '@mdx-js/react';
-import Highlight, { defaultProps } from 'prism-react-renderer';
-import theme from 'prism-react-renderer/themes/nightOwl';
 import React from 'react';
+import { Code } from './src/components/pageElements/Code';
 
 const components = {
-  pre: props => {
-    const className = props.children.props.className || '';
-    const matches = className.match(/language-(?<lang>.*)/);
-
-    return (
-      <Highlight
-        {...defaultProps}
-        code={props.children.props.children.trim()}
-        language={
-          matches && matches.groups && matches.groups.lang
-            ? matches.groups.lang
-            : ''
-        }
-        theme={theme}>
-        {({
-          className,
-          style,
-          tokens,
-          getLineProps,
-          getTokenProps,
-        }) => (
-          <pre className={className} style={style}>
-            {tokens.map((line, i) => (
-              <div {...getLineProps({ line, key: i })}>
-                {line.map((token, key) => (
-                  <span {...getTokenProps({ token, key })} />
-                ))}
-              </div>
-            ))}
-          </pre>
-        )}
-      </Highlight>
-    );
+  h2: ( { children } ) => (
+  <h2 style={{color: 'rebeccapurple'}}>{children}</h2>
+  ),
+  'p.inlineCode': props => (
+     <code {...props} style={{ backgroundColor: 'lightgrey'}} {...props} />
+  ),
+  pre: ({children: {props}}) => {
+    if (props.mdxType === 'code') {
+     return (
+      <Code 
+         codeString={props.children.trim()}
+         language={
+           props.className && 
+           props.className.replace('language-','')
+         }
+         {...props}
+         />
+     )
+    }
   },
-};
-
+}
 export const wrapRootElement = ({ element }) => (
   <MDXProvider components={components}>{element}</MDXProvider>
 );
